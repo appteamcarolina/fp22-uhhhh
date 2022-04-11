b9cc16e968f2032b9ff923f14eaa4cd8c0abe926
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-11 15:15:25 -0400

    project created

---
+++ fp22-happ/fp22-happ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fp22-happ
+//
+//  Created by Bailey Van Wormer on 4/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fp22-happ/fp22-happ/fp22_happApp.swift
@@ -0,0 +1,17 @@
+//
+//  fp22_happApp.swift
+//  fp22-happ
+//
+//  Created by Bailey Van Wormer on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct fp22_happApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
