cb494262d5bf70af79f80dc431fce8fa5c1a21ec
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-11 23:00:46 -0400

    Added 3 new view screens

---
+++ fp22-happ/fp22-happ/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  fp22-happ
+//
+//  Created by Bailey Van Wormer on 4/11/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
