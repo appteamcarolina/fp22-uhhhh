59a326175baca238d8d357b64f9d0d93f1b7f25b
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-12 23:49:05 -0400

    Made the first draft of WelcomeView

--- fp22-happ/fp22-happ/WelcomeView.swift
+++ fp22-happ/fp22-happ/WelcomeView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Spacer()
+            
+            Text("Happ")
+                .font(.custom("Kollektif-Bold", size: 40))
+            Button {
+                //
+            } label: {
+                Text("Get Started")
+                    .bold()
+                    .padding(.vertical, 6)
+                    .padding(.horizontal, 12)
+                    .background(.white)
+                    .cornerRadius(8)
+                    .foregroundColor(.primary)
+                    .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+            }
+            
+            Spacer()
+            
+            Text("Stay Updated On Events Happening Near You")
+                .font(.subheadline)
+                .fontWeight(.thin)
+
+            Spacer()
+            
+        }
     }
 }
 
