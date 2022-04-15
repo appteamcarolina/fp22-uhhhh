839e60f39c26bd15ca2b20f8235e738a533489ce
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-15 15:08:32 -0400

    Started UI for SearchView and AccountView

--- fp22-happ/fp22-happ/AccountView.swift
+++ fp22-happ/fp22-happ/AccountView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text("Account")
+        VStack(alignment: .center) {
+            Image("alecbby")
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 120, alignment: .center)
+            .clipShape(Circle())
+            .padding(.top)
+            
+            Text("Alec Alec")
+                .font(.title)
+            
+            
+            HStack {
+                Text("Saved Events")
+                    .font(.title)
+                    .bold()
+            }
+            
+        }
     }
 }
 
--- fp22-happ/fp22-happ/SearchView.swift
+++ fp22-happ/fp22-happ/SearchView.swift
@@ -9,7 +9,128 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        Text("Search")
+        VStack {
+            
+            Text("Happening Near You")
+                .font(.title)
+                .bold()
+            HStack {
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🎵")
+                    }
+                    
+                    Text("Concerts")
+                }
+                .padding()
+                
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🎭")
+                    }
+                    
+                    Text("Events")
+                }
+                .padding()
+                
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🍾")
+                    }
+                    
+                    Text("Parties")
+                }
+                .padding()
+            }
+            .padding()
+            
+            HStack {
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                        .frame(width: 75, height: 50)
+                        .overlay {
+                            Text("👨🏼‍💻")
+                        }
+                    
+                    Text("Company")
+                }
+                .padding()
+                
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("📝")
+                    }
+                    
+                    Text("Clubs")
+                }
+                .padding()
+                
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🎤")
+                    }
+                    
+                    Text("Speakers")
+                }
+                .padding()
+            }
+            .padding()
+            
+            HStack {
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🏈")
+                    }
+                    
+                    Text("Sports")
+                }
+                .padding()
+                
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🍕")
+                    }
+                    
+                    Text("Food")
+                }
+                .padding()
+                
+                VStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.909))
+                    .frame(width: 75, height: 50)
+                    .overlay {
+                        Text("🙏🏻")
+                    }
+                    
+                    Text("Religious")
+                }
+                .padding()
+            }
+            .padding()
+        }
     }
 }
 
