ee3af245de4d0545c1004bf4051712b58bb0fee0
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-04-19 11:24:51 -0400

    Added "Add Event" Button in ZStack over MapView

--- fp22-happ/fp22-happ/Views/MapView.swift
+++ fp22-happ/fp22-happ/Views/MapView.swift
@@ -11,8 +11,29 @@ import MapKit
 struct MapView: View {
     @StateObject private var vm = MapViewModel()
     var body: some View {
-        Map(coordinateRegion: $vm.region,showsUserLocation: true).onAppear {
-            vm.checkIfLocationServicesIsEnabled()
+    
+        ZStack {
+            
+            Map(coordinateRegion: $vm.region,showsUserLocation: true).onAppear {
+                vm.checkIfLocationServicesIsEnabled()
+            }
+            
+            Button {
+                // Pull up event sheet
+            } label: {
+                
+                ZStack {
+                    
+                    Circle()
+                        .foregroundColor(.white)
+                    .frame(width: 75, height: 75, alignment: .bottomTrailing)
+                    
+                    Image(systemName: "plus")
+                        .font(.system(size: 25))
+                        .foregroundColor(.black)
+                }
+            }
+            .frame(width: 350, height: 750, alignment: .bottomTrailing)
         }
     }
 }
